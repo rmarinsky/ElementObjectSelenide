b846930b65cbcd5845b0f7023962841f4fa2c5a2
bravoaqa <bravoaqa@gmail.com>
2016-10-28 15:53:50 +0300

    update pasw

--- src/main/java/gsmserver/Utils/DefaultData.java
+++ src/main/java/gsmserver/Utils/DefaultData.java
@@ -4,7 +4,7 @@ public class DefaultData {
 
     static final String defaultBaseUrl = "http://rc.gsmserver.com";
     public static final String defaultEmail = "email@test.com";
-    public static final String defaultPassword = "1111";
+    public static final String defaultPassword = "111111";
     public static final Integer avrgCostProduct = 834632;
     public static final Integer defaultProductId = 858049;
     public static final Integer octopusProduct = 832722;
